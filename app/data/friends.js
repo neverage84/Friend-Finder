58b11d7e0f0966e7d171c074c9ff51d7090f5d7c
Noelle Everage <neverage84@yahoo.com>
2019-11-23 20:51:10 -0800

    Added character objects to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,56 @@
+characters = [
+    {
+        "name": "Leia",
+        "photo": "../public/images/profile/leia.jpeg"
+        "scores": [
+            4
+        ]
+    },
+    {
+        "name": "Anakin",
+        "photo": "../public/images/profile/anakin.jpeg"
+        "scores": [
+            5
+        ]
+    },
+    {
+        "name": "Lando",
+        "photo": "../public/images/profile/lando.jpeg"
+        "scores": [
+            4
+        ]
+    },
+    {
+        "name": "Luke",
+        "photo": "../public/images/profile/luke.jpeg"
+        "scores": [
+            4
+        ]
+    },
+    {
+        "name": "Yoda",
+        "photo": "../public/images/profile/yoda.jpeg"
+        "scores": [
+            1
+        ]
+    }
+
+]
+
+// {
+//     "name":"Ahmed",
+//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//     "scores":[
+//         5,
+//         1,
+//         4,
+//         4,
+//         5,
+//         1,
+//         2,
+//         5,
+//         4,
+//         1
+//       ]
+//   }
+  

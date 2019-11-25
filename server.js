8f0061d8be8c73d498f68ddfaa9369c1a16a9073
Noelle Everage <neverage84@yahoo.com>
2019-11-24 22:09:30 -0800

    Added pathways on htmlroutes

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+ 
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
 
